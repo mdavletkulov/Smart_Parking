a53e26c9e2d259c1390bbbd1d6a8c81823fc6860
Maksim Davletkulov <m160598@gmail.com>
2020-05-25 12:58:45 +0400

    Main page completed

--- src/main/java/com/example/smartParking/config/WebSecurityConfig.java
+++ src/main/java/com/example/smartParking/config/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/registration", "/static/**", "/report/**", "/activate/**", "/download/report/**").permitAll()
+                    .antMatchers("/", "/registration", "/static/**", "/report/**", "/activate/**", "/download/report/**", "/img/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                 .formLogin()
